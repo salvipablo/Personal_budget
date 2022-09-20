17b0bcafbfa692d7cc065eceaff5fe3e7cd45b16
psalvi <salvipablo@gmail.com>
2022-09-20 09:50:54 -0300

    Users
    Routes and controllers for basic CRUD
    operations were created. The possibility
    of bringing all the users or searching
    individually was also incorporated.
    
    Resolve: T#0006

--- back_pres/src/controllers/users.controller.js
+++ back_pres/src/controllers/users.controller.js
@@ -9,6 +9,21 @@ export const getUsers = async (req, res) => {
   }
 };
 
+export const getUser = async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const user = await UserSchema.findOne({ where: { username: username } });
+
+    if (!user) 
+      return res.status(404).json({ message: 'Searched user does not exist' })
+
+    res.json(user);
+  } catch (error) {
+    return res.status(500).json({ message: message.error })
+  }
+};
+
 export const createUser = async (req, res) => {
   const { username, password } = req.body;
 
@@ -21,5 +36,39 @@ export const createUser = async (req, res) => {
     res.json(newUser);
   } catch (error) {
     return res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { username, password } = req.body;
+
+  try {
+    const foundUser = await UserSchema.findByPk(id);
+
+    foundUser.username = username;
+    foundUser.password = password;
+
+    await foundUser.save();
+
+    res.json(foundUser);
+  } catch (error) {
+    return res.status(500).json({ message: message.error })
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    UserSchema.destroy({
+      where: {
+        id: id
+      }
+    });
+  
+    res.sendStatus(204);
+  } catch (error) {
+    return res.status(500).json({ message: message.error })
   }
 };
--- back_pres/src/routes/users.routes.js
+++ back_pres/src/routes/users.routes.js
@@ -1,13 +1,19 @@
 import { Router } from "express"
 
-import { getUsers, createUser } from '../controllers/users.controller.js';
+import { 
+  getUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUser
+} from '../controllers/users.controller.js';
 
 const router = Router();
 
 router.get('/users', getUsers);
+router.get('/users/:username', getUser);
 router.post('/users', createUser);
-router.put('/users/:id');
-router.delete('/users/:id');
-router.get('/users/:id');
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router;
