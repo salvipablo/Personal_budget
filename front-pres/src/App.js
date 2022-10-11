23e0efd42bdcb487d0d9e89a40c7dc4845c22eb5
psalvi <salvipablo@gmail.com>
2022-10-11 16:14:41 -0300

    basic routing
    Resolve: T#0012

--- front-pres/src/App.js
+++ front-pres/src/App.js
@@ -1,8 +1,24 @@
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Home from './pages/Home/index.js';
+import Login from "./pages/Login/index.js";
+import Register from "./pages/Register/index.js";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Presupuesto Personal.</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
