e37b84afb65e5140aec6257a1e34ea4fd40b0548
psalvi <salvipablo@gmail.com>
2022-10-27 08:41:21 -0300

    Sign off
    Resolve: T#0032

--- front-pres/src/components/Header/index.js
+++ front-pres/src/components/Header/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './index.css';
 
 import iconHeader from '../../Assets/images/iconHeader.png';
 
 function BudgetHeader() {
+  let navigate = useNavigate();
+
+  const handleSignOff = async (e) => {
+    sessionStorage.removeItem("userBudget");
+    sessionStorage.removeItem("emailBudget");
+    sessionStorage.removeItem("idBudget");
+    navigate('/');
+  };
+
   return (
     <header className='headerBudget'>
       <div className='cntHeaderContent'>
@@ -13,7 +23,10 @@ function BudgetHeader() {
           <h1 className='titleHeader'>Presupuesto Personal</h1>
         </div>
         <div className='cntHeaderRight'>
-          <button className='btnSession'>Cerrar Sesion</button>
+          <button
+            className='btnSession'
+            onClick={(e) => handleSignOff(e)}
+          >Cerrar Sesion</button>
         </div>
       </div>
     </header>
